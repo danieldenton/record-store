d72249d40f018362a61f13e3979041c8a1e17ac8
Daniel Denton <96963270+danieldenton@users.noreply.github.com>
2024-09-01 14:30:52 -0700

    shacn added

--- src/app/create-account/page.tsx
+++ src/app/create-account/page.tsx
@@ -1,10 +1,10 @@
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: 'Create Account',
+  title: 'Genre',
 };
 
 
 export default function CreateAccount() {
-  return <h1>Create Account</h1>;
+  return <h1>Genre Page</h1>;
 }
