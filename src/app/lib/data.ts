04d9eded36182df98a91d54db5b7900adb4485a9
Daniel Denton <96963270+danieldenton@users.noreply.github.com>
2024-09-05 08:50:34 -0700

    commas added

--- src/app/lib/data.ts
+++ src/app/lib/data.ts
@@ -5,14 +5,14 @@ export async function fetchSearch(query: string) {
   try {
     const results = await sql<Album & { match: string }>`
         SELECT
-          albums.id
-          albums.artist
-          albums.name
-          albums.year
-          albums.notes
-          albums.price
-          albums.cover
-          albums.genre
+          albums.id,
+          albums.artist,
+          albums.name,
+          albums.year,
+          albums.notes,
+          albums.price,
+          albums.cover,
+          albums.genre,
         CASE
           WHEN albums.name ILIKE ${`%${query}%`} THEN 'name'
           WHEN albums.artist ILIKE ${`%${query}%`} THEN 'artist'
