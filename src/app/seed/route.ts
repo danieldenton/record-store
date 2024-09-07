e1ca62d25a032deca9123b70025abb17ea03995a
Daniel Denton <96963270+danieldenton@users.noreply.github.com>
2024-09-06 19:41:17 -0700

    updated seed

--- src/app/seed/route.ts
+++ src/app/seed/route.ts
@@ -5,17 +5,7 @@ import { artists } from "../lib/artist-data";
 const client = await db.connect();
 
 async function seedArtists() {
-  await client.sql`
-    CREATE TABLE IF NOT EXISTS artists (
-      id SERIAL PRIMARY KEY,
-      name VARCHAR(255) NOT NULL,
-      bio VARCHAR(255) NOT NULL,
-      image VARCHAR(255) NOT NULL,
-      genres TEXT[]
-    );
-  `;
-
-  await Promise.all(
+await Promise.all(
     artists.map(
       (artist) => client.sql`
         INSERT INTO artists (id, name, bio, image, genres)
@@ -27,24 +17,11 @@ async function seedArtists() {
 }
 
 async function seedAlbums() {
-  await client.sql`
-    CREATE TABLE IF NOT EXISTS albums (
-      id SERIAL PRIMARY KEY,
-      name VARCHAR(255) NOT NULL,
-      release VARCHAR(255) NOT NULL,
-      notes VARCHAR(255) NOT NULL,
-      price VARCHAR(255) NOT NULL,
-      cover VARCHAR(255) NOT NULL,
-      genres TEXT[],
-      artist_id INTEGER REFERENCES artists(id) ON DELETE SET NULL
-    );
-  `;
-
   await Promise.all(
     albums.map(
       (album) => client.sql`
         INSERT INTO albums (id, name, release, notes, price, cover, genres, artist_id)
-        VALUES (${album.id}, ${album.name}, ${album.release}, ${album.notes}, ${album.price}, ${album.cover}, ${album.genres}, ${album.artistId})
+        VALUES (${album.id}, ${album.name}, ${album.release}, ${album.notes}, ${album.price}, ${album.cover}, ${album.genres}, ${album.artistIds})
         ON CONFLICT (id) DO NOTHING;
       `
     )
