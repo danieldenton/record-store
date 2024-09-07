56e674c17b3741a9a1dcefd025ffd104725f0340
Daniel Denton <96963270+danieldenton@users.noreply.github.com>
2024-09-06 20:02:46 -0700

    new seeding complete

--- src/app/seed/route.ts
+++ src/app/seed/route.ts
@@ -5,26 +5,48 @@ import { artists } from "../lib/artist-data";
 const client = await db.connect();
 
 async function seedArtists() {
-await Promise.all(
-    artists.map(
-      (artist) => client.sql`
+  await Promise.all(
+    artists.map((artist) => {
+      const genresArray = `{${artist.genres.join(",")}}`; // Convert to PostgreSQL array format
+      return client.sql`
         INSERT INTO artists (id, name, bio, image, genres)
-        VALUES (${artist.id}, ${artist.name}, ${artist.bio}, ${artist.image}, ${artist.genres})
+        VALUES (${artist.id}, ${artist.name}, ${artist.bio}, ${artist.image}, ${genresArray})
         ON CONFLICT (id) DO NOTHING;
-      `
-    )
+      `;
+    })
   );
 }
 
 async function seedAlbums() {
   await Promise.all(
-    albums.map(
-      (album) => client.sql`
-        INSERT INTO albums (id, name, release, notes, price, cover, genres, artist_id)
-        VALUES (${album.id}, ${album.name}, ${album.release}, ${album.notes}, ${album.price}, ${album.cover}, ${album.genres}, ${album.artistIds})
+    albums.map((album) => {
+      const genresArray = album.genres ? `{${album.genres.join(",")}}` : "{}";
+      return client.sql`
+        INSERT INTO albums (id, name, release, notes, price, cover, genres)
+        VALUES (${album.id}, ${album.name}, ${album.release}, ${album.notes}, ${album.price}, ${album.cover}, ${genresArray})
         ON CONFLICT (id) DO NOTHING;
-      `
-    )
+      `;
+    })
+  );
+}
+
+async function seedAlbumArtists() {
+  await Promise.all(
+    albums.map((album) => {
+      if (album.artistIds) {
+        return Promise.all(
+          album.artistIds.map((artistId) =>
+            client.sql`
+              INSERT INTO album_artists (album_id, artist_id)
+              VALUES (${album.id}, ${artistId})
+              ON CONFLICT (album_id, artist_id) DO NOTHING;
+            `
+          )
+        );
+      } else {
+        return Promise.resolve();
+      }
+    })
   );
 }
 
@@ -33,11 +55,17 @@ export async function GET() {
     await client.sql`BEGIN`;
     await seedArtists();
     await seedAlbums();
+    await seedAlbumArtists();
     await client.sql`COMMIT`;
 
-    return Response.json({ message: "Database seeded successfully" });
+    return new Response(
+      JSON.stringify({ message: "Database seeded successfully" }),
+      { status: 200 }
+    );
   } catch (error) {
     await client.sql`ROLLBACK`;
-    return Response.json({ error }, { status: 500 });
+    return new Response("There was an error seeding you data", {
+      status: 500,
+    });
   }
 }
