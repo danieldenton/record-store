fe70874f742d75da419b9ba63dac4d577931f5b1
Daniel Denton <96963270+danieldenton@users.noreply.github.com>
2024-09-06 22:40:33 -0700

    getAlbumByID seems done. to be tested

--- src/app/lib/data.ts
+++ src/app/lib/data.ts
@@ -1,5 +1,5 @@
 import { sql } from "@vercel/postgres";
-import { ArtistSearchResult, AlbumSearchResult } from "./definitions";
+import { ArtistSearchResult, AlbumSearchResult, Album } from "./definitions";
 
 export async function fetchSearch(query: string) {
   try {
@@ -36,5 +36,50 @@ export async function fetchSearch(query: string) {
   } catch (error) {
     console.error("Database Error:", error);
     throw new Error("Failed to fetch results.");
+  }
+}
+
+export async function fetchAlbumById(id: string) {
+  try {
+    const album = await sql<Album>`
+        SELECT
+          id,
+          name,
+          release,
+          notes,
+          price,
+          cover,
+          genres,
+        FROM albums
+        WHERE albums.id = ${id};
+      `;
+
+    const artistQuery = await sql`
+        SELECT 
+          artist_id,
+        FROM album_artists
+        WHERE album_artists.album_id = ${id}  
+      `;
+
+    const artistIds = artistQuery.rows.map((row) => row.artist_id);
+
+    const artistIdsString = `{${artistIds.join(",")}}`;
+
+    const artistNamesQuery = await sql`
+        SELECT 
+          name,
+        FROM artists
+        WHERE id = ANY(${artistIdsString});
+  `;
+
+    const artist = artistNamesQuery.rows.map((row) => row.name);
+
+    return {
+      ...album,
+      artist,
+    };
+  } catch (error) {
+    console.error("Database Error:", error);
+    throw new Error("Failed to fetch album.");
   }
 }
--- src/app/ui/search-results.tsx
+++ src/app/ui/search-results.tsx
@@ -1,17 +1,23 @@
 import { SearchResult } from "../lib/definitions";
 import Link from "next/link";
 
-export default async function SearchResults({
+export default function SearchResults({
   searchResults,
 }: {
   searchResults: SearchResult[];
 }) {
-  const results = searchResults?.map((result) => {
-    return (
-      <Link href={`${result.type}`} key={result.id} className="flex flex-row text-primary hover:bg-popover">
-        <p>{result.name}</p> <p className="text-destructive text-sm ml-2 mt-0.5">{result.type}</p>
-      </Link>
-    );
-  });
-  return <div className="bg-secondary">{results}</div>;
+  return (
+    <div className="bg-secondary">
+      {searchResults.map((result) => (
+        <Link
+          href={`/${result.type}/${result.id}`}
+          key={result.id}
+          className="flex flex-row text-primary hover:bg-popover"
+        >
+          <p>{result.name}</p>
+          <p className="text-destructive text-sm ml-2 mt-0.5">{result.type}</p>
+        </Link>
+      ))}
+    </div>
+  );
 }
