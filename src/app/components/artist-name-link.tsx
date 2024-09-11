88559cf45e87c7758c04f268fd7bb465114b9b84
Daniel Denton <96963270+danieldenton@users.noreply.github.com>
2024-09-11 12:52:10 -0700

    return albumById now returns an artist image as well

--- src/app/components/artist-name-link.tsx
+++ src/app/components/artist-name-link.tsx
@@ -1,16 +1,13 @@
 import Link from "next/link";
 import Image from "next/image";
-import { fetchArtistById } from "../lib/data";
 
 type Artist = {
   id: string;
   name: string;
-  // image: string
+  image: string;
 };
 
 export default function ArtistNameLink({ artists }: { artists: Artist[] }) {
-  // const artist = await fetchArtistById(id);
-  // const image = artist.image ? artist.image : artist.albums[0].cover || "";
   const nameLinks = artists.map((artist) => {
     return (
       <Link href={`/artist/${artist.id}`} className="flex items-center m-1">
--- src/app/components/image-not-found.tsx
+++ src/app/components/image-not-found.tsx
@@ -1,7 +1,14 @@
-export default function ImageNotFound() {
+type Size = {
+  size?: number;
+};
+
+export default function ImageNotFound({ size }: Size) {
   return (
-    <div className="flex justify-center items-center bg-secondary h-[300px] w-[300px]">
-      <h1 className="text-primary">Image not found</h1>
+    <div
+      className="flex justify-center items-center bg-secondary"
+      style={{ height: size, width: size }}
+    >
+      <h1 className="text-primary text-xs">Image not found</h1>
     </div>
   );
 }
