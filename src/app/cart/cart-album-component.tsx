e513b48c32ea277daa1a38d4d1de5c0de6de6e25
Daniel Denton <96963270+danieldenton@users.noreply.github.com>
2024-09-11 15:35:02 -0700

    multiple artists handled in cart

--- src/app/cart/cart-album-component.tsx
+++ src/app/cart/cart-album-component.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 
 import Image from "next/image";
 import { useCartContext } from "@/context/cart";
@@ -6,11 +6,18 @@ import { CartAlbum } from "../lib/definitions";
 
 export default function CartAlbumComponent({ album }: { album: CartAlbum }) {
   const { cart, setCart } = useCartContext();
-  
+
+  const artistArray = album.artists.map((artist) => {
+    return artist;
+  });
+  const artists =
+    artistArray.length === 1 ? artistArray[0] : artistArray.join(", ");
+
   return (
     <div className="flex items-center m-1">
       <Image src={album.cover} alt={album.name} height={50} width={50} />
-       <p className="ml-3">{album.name}</p>
+      <p className="ml-3">{album.name}</p>
+      <p className="ml-3">{artists}</p>
       <p className="ml-3">{album.price}</p>
     </div>
   );
--- src/app/cart/cart-page-component.tsx
+++ src/app/cart/cart-page-component.tsx
@@ -2,9 +2,7 @@ import CartAlbumComponent from "./cart-album-component";
 import { CartAlbum } from "../lib/definitions";
 
 export default function CartPageComponent({ albums }: { albums: CartAlbum[] }) {
-  console.log(albums);
   const cartItems = albums.map((item) => {
-    console.log("Page prop:", item);
     return <CartAlbumComponent album={item} />;
   });
   return <>{cartItems}</>;
