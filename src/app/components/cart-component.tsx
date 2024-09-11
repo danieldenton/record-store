b5d268bf25f9c5f00355f3817d57a2d5ac4982fc
Daniel Denton <96963270+danieldenton@users.noreply.github.com>
2024-09-11 11:56:31 -0700

    cart working correctly

--- src/app/components/cart-component.tsx
+++ src/app/components/cart-component.tsx
@@ -5,10 +5,14 @@ import { ShoppingCartIcon } from "@heroicons/react/24/outline";
 import { useCartContext } from "@/context/cart";
 export default function CartComponent() {
   const { cart } = useCartContext();
-const numberOfCartItems = cart.length
+  const numberOfCartItems = cart.length;
   return (
-    <Link href="/cart" className="mr-1">
-      <div></div>
+    <Link href="/cart" className="relative mr-2 h-10">
+      {numberOfCartItems > 0 ? (
+        <div className="absolute top-0 right-0 h-6 w-6 transform translate-x-1/4 -translate-y-1/4 rounded-full bg-red text-center">
+          {numberOfCartItems}
+        </div>
+      ) : null}
       <ShoppingCartIcon className="h-10" />
     </Link>
   );
