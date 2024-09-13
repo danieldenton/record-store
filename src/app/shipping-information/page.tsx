c7ab4f12172bac0f03f47638ed6a2a50cf952deb
Daniel Denton <96963270+danieldenton@users.noreply.github.com>
2024-09-13 15:43:19 -0700

    shipping page

--- src/app/shipping-information/page.tsx
+++ src/app/shipping-information/page.tsx
@@ -2,7 +2,7 @@ import ShippingForm from "./shipping-form";
 
 export default function ShippingInformation() {
   return (
-    <div className="m-20 ">
+    <div className="m-20">
       <ShippingForm />
     </div>
   );
