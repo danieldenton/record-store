43131859aab91815a0c148d58f404db83629f8ae
Daniel Denton <96963270+danieldenton@users.noreply.github.com>
2024-09-14 18:19:12 -0700

    red adjusted

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -19,7 +19,7 @@ const config: Config = {
         white: " #e5e5e5",
         yellow: "rgb(234, 255, 0)",
         grey: "#BDBDBD",
-        red: "#B22222",
+        red: "#CC3333",
       },
       animation: {
         fade: "fadeOut 1.5s ease-in-out",
