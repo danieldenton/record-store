9c03284fb548260aaddcfd9b6380a5ee0f13c40a
Daniel Denton <96963270+danieldenton@users.noreply.github.com>
2024-09-14 18:11:51 -0700

    buttons working and navigating correctly

--- src/app/shipping-information/shipping-form.tsx
+++ src/app/shipping-information/shipping-form.tsx
@@ -9,6 +9,7 @@ export default function ShippingForm() {
   const { cartParam } = useCartContext();
   const [shippingAddress, setShippingAddress] = useState({});
   const [billingAddress, setBillingAddress] = useState({});
+  const [billing, setBilling] = useState(false);
   const [formData, setFormData] = useState({
     name: "",
     street1: "",
@@ -21,14 +22,17 @@ export default function ShippingForm() {
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    const action = (e.nativeEvent as SubmitEvent).submitter?.getAttribute("value")
+    const action = (e.nativeEvent as SubmitEvent).submitter?.getAttribute(
+      "value"
+    );
     console.log(action);
     if (action === "continue_to_checkout") {
       setShippingAddress(formData);
       setBillingAddress(formData);
       router.push(`/checkout?checkout=${encodeURIComponent(cartParam)}`);
-    } else {
+    } else if (action === "enter_billing_address") {
       setShippingAddress(formData);
+      setBilling(true);
       setFormData({
         name: "",
         street1: "",
@@ -38,6 +42,9 @@ export default function ShippingForm() {
         country: "",
         zip: "",
       });
+    } else {
+      setBillingAddress(formData);
+      router.push(`/checkout?checkout=${encodeURIComponent(cartParam)}`);
     }
   };
 
@@ -85,7 +92,7 @@ export default function ShippingForm() {
       value: formData.zip,
     },
   ];
-  console.log(shippingAddress, billingAddress);
+
   const shippingFormInputs = formInputData.map((input) => {
     return (
       <label className="flex flex-col font-bold text-center my-2">
@@ -104,26 +111,43 @@ export default function ShippingForm() {
   return (
     <div className="flex flex-col justify-center border-2 p-6 w-1/2 rounded">
       <div className="text-center">
-        <p className="font-bold text-xl">Shipping Address</p>
+        {billing ? (
+          <p className="font-bold text-xl">Billing Address</p>
+        ) : (
+          <p className="font-bold text-xl">Shipping Address</p>
+        )}
       </div>
       <form className="flex flex-col w-full h-full p-6" onSubmit={handleSubmit}>
         {shippingFormInputs}
-        <button
-          type="submit"
-          name="action"
-          value="continue_to_checkout"
-          className="mt-6 p-2.5 bg-red text-white rounded"
-        >
-          Billing Address is the Same as Shipping
-        </button>
-        <button
-          type="submit"
-          name="action"
-          value="enter_billing_address"
-          className="mt-6 p-2.5 bg-red text-white rounded"
-        >
-          Enter Billing Address
-        </button>
+        {billing ? (
+          <button
+            type="submit"
+            name="action"
+            value="checkout"
+            className="mt-6 p-2.5 bg-red text-white rounded"
+          >
+            Checkout
+          </button>
+        ) : (
+          <>
+            <button
+              type="submit"
+              name="action"
+              value="continue_to_checkout"
+              className="mt-6 p-2.5 bg-red text-white rounded"
+            >
+              Billing Address is the Same as Shipping
+            </button>
+            <button
+              type="submit"
+              name="action"
+              value="enter_billing_address"
+              className="mt-6 p-2.5 bg-red text-white rounded"
+            >
+              Enter Billing Address
+            </button>
+          </>
+        )}
       </form>
     </div>
   );
