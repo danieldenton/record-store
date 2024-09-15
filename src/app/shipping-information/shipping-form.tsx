2bea59c97850a51119179f570e4fc983de7617c2
Daniel Denton <96963270+danieldenton@users.noreply.github.com>
2024-09-14 18:02:34 -0700

    handleSubmit complete

--- src/app/shipping-information/shipping-form.tsx
+++ src/app/shipping-information/shipping-form.tsx
@@ -1,22 +1,45 @@
 "use client";
 
 import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { useCartContext } from "@/context/cart";
 
 export default function ShippingForm() {
+  const router = useRouter();
+  const { cartParam } = useCartContext();
+  const [shippingAddress, setShippingAddress] = useState({});
+  const [billingAddress, setBillingAddress] = useState({});
   const [formData, setFormData] = useState({
     name: "",
-    address1: "",
-    address2: "",
+    street1: "",
+    street2: "",
     city: "",
     state: "",
+    country: "",
     zip: "",
-    billingName: "",
-    billingAddress1: "",
-    billingAddress2: "",
-    billingCity: "",
-    billingState: "",
-    billingZip: "",
   });
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const action = (e.nativeEvent as SubmitEvent).submitter?.getAttribute("value")
+    console.log(action);
+    if (action === "continue_to_checkout") {
+      setShippingAddress(formData);
+      setBillingAddress(formData);
+      router.push(`/checkout?checkout=${encodeURIComponent(cartParam)}`);
+    } else {
+      setShippingAddress(formData);
+      setFormData({
+        name: "",
+        street1: "",
+        street2: "",
+        city: "",
+        state: "",
+        country: "",
+        zip: "",
+      });
+    }
+  };
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setFormData({
@@ -34,30 +57,35 @@ export default function ShippingForm() {
     {
       label: "Street 1",
       name: "street1",
-      value: formData.name,
+      value: formData.street1,
     },
     {
       label: "Street 2",
       name: "street",
-      value: formData.name,
+      value: formData.street2,
     },
     {
       label: "City",
       name: "city",
-      value: formData.name,
+      value: formData.city,
     },
     {
       label: "State",
       name: "state",
-      value: formData.name,
+      value: formData.state,
+    },
+    {
+      label: "Country",
+      name: "country",
+      value: formData.country,
     },
     {
       label: "Zip",
       name: "zip",
-      value: formData.name,
+      value: formData.zip,
     },
   ];
-
+  console.log(shippingAddress, billingAddress);
   const shippingFormInputs = formInputData.map((input) => {
     return (
       <label className="flex flex-col font-bold text-center my-2">
@@ -78,10 +106,23 @@ export default function ShippingForm() {
       <div className="text-center">
         <p className="font-bold text-xl">Shipping Address</p>
       </div>
-      <form className="flex flex-col w-full h-full p-6">
+      <form className="flex flex-col w-full h-full p-6" onSubmit={handleSubmit}>
         {shippingFormInputs}
-        <button type="submit" className="mt-6 p-2.5 bg-red text-white rounded">
-          Submit
+        <button
+          type="submit"
+          name="action"
+          value="continue_to_checkout"
+          className="mt-6 p-2.5 bg-red text-white rounded"
+        >
+          Billing Address is the Same as Shipping
+        </button>
+        <button
+          type="submit"
+          name="action"
+          value="enter_billing_address"
+          className="mt-6 p-2.5 bg-red text-white rounded"
+        >
+          Enter Billing Address
         </button>
       </form>
     </div>
